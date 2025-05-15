c228773a56ea2af105229a6748fd9f60c0a07f85
sundarrajboobalan <73453612+sundarrajboobalan@users.noreply.github.com>
2025-05-15 14:45:02 +0530

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello {
   
-  public static String GREETING = "Hello world!!!";
+  public static String GREETING = "Welocme to GitHub Actions!!!";
   
   public static void main(String []args) {
     
